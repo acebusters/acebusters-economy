2a6b8b98f0972dccc2303f94e4f79d81adbdc908
Kosta Korenkov <kosta@whoot.me>
2017-08-25 21:55:14 +0400

    WIP: fix tests (#15)
    * chore: reuse INFINITY variable
    
    * test: floor price should adjust automatically with NTZ inflation
    
    * chore: make INFINITY constant
    
    * tests: few purchase tests
    
    * docs: somehow it is possible to run individual test files now
    
    * tests: fix erc223 test
    
    * tests: sell by bloody high price

--- contracts/controller/MarketEnabled.sol
+++ contracts/controller/MarketEnabled.sol
@@ -5,30 +5,33 @@ import "./NutzEnabled.sol";
 
 contract MarketEnabled is NutzEnabled {
 
+  uint256 constant INFINITY = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+
   // address of the pull payemnt satelite
   address public pullAddr;
-
-  function MarketEnabled(address _pullAddr, address _storageAddr, address _nutzAddr) 
-    NutzEnabled(_nutzAddr, _storageAddr) {
-    pullAddr = _pullAddr;
-  }
 
   // the Token sale mechanism parameters:
   // purchasePrice is the number of NTZ received for purchase with 1 ETH
   uint256 internal purchasePrice;
 
-  function ceiling() constant returns (uint256) {
-    return purchasePrice;
-  }
   // floor is the number of NTZ needed, to receive 1 ETH in sell
   uint256 internal salePrice;
 
+  function MarketEnabled(address _pullAddr, address _storageAddr, address _nutzAddr)
+    NutzEnabled(_nutzAddr, _storageAddr) {
+    pullAddr = _pullAddr;
+  }
+
+
+  function ceiling() constant returns (uint256) {
+    return purchasePrice;
+  }
 
   // returns either the salePrice, or if reserve does not suffice
   // for active supply, returns maxFloor
   function floor() constant returns (uint256) {
     if (this.balance == 0) {
-      return 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff; // infinity
+      return INFINITY;
     }
     uint256 maxFloor = activeSupply().mul(1000000).div(this.balance); // 1,000,000 WEI, used as price factor
     // return max of maxFloor or salePrice
@@ -39,13 +42,12 @@ contract MarketEnabled is NutzEnabled {
     require(_newPurchasePrice <= salePrice);
     purchasePrice = _newPurchasePrice;
   }
-  
+
   function moveFloor(uint256 _newSalePrice) public onlyAdmins {
     require(_newSalePrice >= purchasePrice);
     // moveFloor fails if the administrator tries to push the floor so low
     // that the sale mechanism is no longer able to buy back all tokens at
     // the floor price if those funds were to be withdrawn.
-    uint256 INFINITY = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
     if (_newSalePrice < INFINITY) {
       require(this.balance >= activeSupply().mul(1000000).div(_newSalePrice)); // 1,000,000 WEI, used as price factor
     }
@@ -79,11 +81,11 @@ contract MarketEnabled is NutzEnabled {
 
   function sell(address _from, uint256 _price, uint256 _amountBabz) public onlyNutz whenNotPaused {
     uint256 effectiveFloor = floor();
-    uint256 INFINITY = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
     require(effectiveFloor != INFINITY);
     require(_price == effectiveFloor);
 
     uint256 amountWei = _amountBabz.mul(1000000).div(effectiveFloor);  // 1,000,000 WEI, used as price factor
+    require(amountWei > 0);
     // make sure power pool shrinks proportional to economy
     uint256 powPool = powerPool();
     uint256 activeSup = activeSupply();
--- test/helpers/ERC223ReceiverMock.sol
+++ test/helpers/ERC223ReceiverMock.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.11;
 
 
 import '../../contracts/ERC223ReceivingContract.sol';
+import '../../contracts/satelites/Nutz.sol';
 
 
 // mock class using StandardToken
@@ -16,8 +17,8 @@ contract ERC223ReceiverMock is ERC223ReceivingContract {
   function () payable {
   }
 
-  function forward(address _to, uint256 _value) {
-  	assert(_to.call.value(_value)());
+  function forward(address _to, uint256 _value, uint256 _price) {
+  	Nutz(_to).purchase.value(_value)(_price);
   }
 
 }
