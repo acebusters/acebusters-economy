d006e6ce745f86822605d1bbb78b105ca9830aa5
Kosta Korenkov <kosta@whoot.me>
2017-09-11 19:36:54 +0300

    fix(deployment): create Controller properly in deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,25 +5,37 @@ var Storage = artifacts.require("./contracts/satelites/Storage.sol");
 var Controller = artifacts.require("./contracts/controller/Controller.sol");
 
 module.exports = function(deployer, network, accounts) {
-  let controller;
-  deployer.deploy([Nutz, Power, PullPayment, Storage]).then(function() {
-    return deployer.deploy(Controller, Power.address, PullPayment.address, Nutz.address, Storage.address);
+  let power, pull, nutz, storage;
+  deployer.deploy(Nutz)
+  .then(function() {
+    return deployer.deploy(Power);
+  }).then(function() {
+    return deployer.deploy(PullPayment);
+  }).then(function() {
+    return deployer.deploy(Storage);
+  }).then(function() {
+    return Nutz.deployed();
+  }).then(function(_nutz) {
+    nutz = _nutz;
+    return Power.deployed();
+  }).then(function(_power) {
+    power = _power;
+    return PullPayment.deployed();
+  }).then(function(_pull) {
+    pull = _pull;
+    return Storage.deployed();
+  }).then(function(_storage) {
+    storage = _storage;
+    return deployer.deploy(Controller, power.address, pull.address, nutz.address, storage.address);
   }).then(function() {
     return Controller.deployed();
-  }).then(function(_controller){
-    controller = _controller;
-    return Nutz.deployed();
-  }).then(function(nutz) {
+  }).then(function(controller){
+    console.log('Transfering satellites\' ownership to Controller..');
     nutz.transferOwnership(controller.address);
-    return Power.deployed();
-  }).then(function(power) {
     power.transferOwnership(controller.address);
-    return PullPayment.deployed();
-  }).then(function(pull) {
     pull.transferOwnership(controller.address);
-    return Storage.deployed();
-  }).then(function(storage) {
     storage.transferOwnership(controller.address);
+    console.log('Unpausing Controller..');
     return controller.unpause();
   });
 };
