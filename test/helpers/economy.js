6144508bbd8534e3d8c8fa7508310dac3378638d
Kosta Korenkov <kosta@whoot.me>
2017-10-12 00:39:02 +0300

    chore: fix formatting for economy helper

--- test/helpers/economy.js
+++ test/helpers/economy.js
@@ -1,13 +1,12 @@
 const fromCents = function fromCents(cents) {
-  return cents.div(Math.pow(10, 12));
+  let val = cents.div(Math.pow(10, 12));
+  return val.toFixed(12);
 }
 
 const printValue = function printValue(cents, ticker) {
   const l = fromCents(cents).toString().length;
-  let tabs = `\t\t\t`;
-  if (l > 13) tabs = '\t\t';
-  if (l >= 19) tabs = '\t';
-  if (l < 4) tabs = '\t\t\t\t';
+  let tabs = `\t\t`;
+  if (l > 14) tabs = '\t';
   return `${fromCents(cents)} ${ticker}${tabs} ${cents}`;
 }
 
@@ -25,7 +24,6 @@ const printEconomy = async function(controller, nutz, power, user) {
     console.log(`User Babz:\t\t ${printValue(await nutz.balanceOf.call(user), 'NTZ')}`);
     let userPower = await power.balanceOf.call(user);
     console.log(`User Power:\t\t ${printValue(userPower, 'ABP')}`);
-    console.log(`Babz for power down?:\t ${printValue(userPower.mul(await controller.completeSupply.call()).div(await controller.authorizedPower.call()), 'NTZ')}`)
   }
   console.log(`---`);
 }
