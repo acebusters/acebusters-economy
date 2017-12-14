31f6666feb24ec0ab1a04c5449f94f9988ff34d9
JM <31426810+Blok-Labs@users.noreply.github.com>
2017-12-14 13:34:34 -0600

    quick fixes

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -7,26 +7,29 @@ pragma solidity ^0.4.11;
  */
 library SafeMath {
   function mul(uint256 a, uint256 b) internal returns (uint256) {
+    require(a >= 0 && b >= 0);
     uint256 c = a * b;
-    assert(a == 0 || c / a == b);
+    require(a == 0 || c / a == b);
     return c;
   }
 
   function div(uint256 a, uint256 b) internal returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    require(b > 0); // Solidity automatically throws when dividing by 0
+    require(a == b * c + a % b); // There is no case in which this doesn't hold
     uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
   function sub(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b <= a);
+    require(a >= 0 && b >= 0);
+    require(b <= a);
     return a - b;
   }
 
   function add(uint256 a, uint256 b) internal returns (uint256) {
+    require(a >= 0 && b >= 0);
+    require(c >= a);
     uint256 c = a + b;
-    assert(c >= a);
     return c;
   }
 }
